4d726258ee051166893c5e2eaf6bbed89e18f003
NOTAHACKER9999 <th15isar0bothi@gmail.com>
2025-09-15 19:43:51 -0500

    Update Duckduckgo.py

--- MATRIX GLITCH/Proxy/Python/Duckduckgo.py
+++ MATRIX GLITCH/Proxy/Python/Duckduckgo.py
@@ -1,9 +1,4 @@
 import js
 
-# Function to open DuckDuckGo in a new tab/window
-def open_duckduckgo():
-    # "_blank" opens a new tab
-    js.window.open("https://duckduckgo.com", "_blank")
-
-# Call the function
-open_duckduckgo()
+# Open DuckDuckGo in a new tab
+js.window.open("https://duckduckgo.com", "_blank")
